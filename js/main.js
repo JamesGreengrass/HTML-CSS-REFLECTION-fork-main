911be6c00e3836de0dc1890f4a30e899a62e73c1
James Greengrass <101417323+JamesGreengrass@users.noreply.github.com>
2022-05-31 17:17:51 +0100

    Adding JS
    Added JS for the scroll header and slide out menu.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
-    document.getElementById('menu-btn').classList.toggle('active')
+    document.getElementById('menu-btn').classList.toggle('is-active');
     document.getElementById('main').classList.toggle('active');
 }

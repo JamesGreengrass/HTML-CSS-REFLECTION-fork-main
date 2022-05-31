2ab297511299a9789a630da80e79a9c1584b9c2e
James Greengrass <101417323+JamesGreengrass@users.noreply.github.com>
2022-05-31 13:09:09 +0100

    Adding Styles To Slide-Out Menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('menu-btn').classList.toggle('active')
+    document.getElementById('main').classList.toggle('active');
+}

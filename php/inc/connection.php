e639db0fff93faa4b2d5026c25bca4e720703b3f
James Greengrass <101417323+JamesGreengrass@users.noreply.github.com>
2022-08-25 17:15:48 +0100

    Changed The Connection
    I have changed the connection to a different database that I have created in PHPmyadmin.
    
    I have also managed to loop through the array and get it to display on the website.

--- php/inc/connection.php
+++ php/inc/connection.php
@@ -1,14 +1,30 @@
 <?php
 
 try {
-    $db = new PDO("sqlite:".__DIR__."/database.db");
+    $db = new PDO("mysql:host=localhost;dbname=database-news;","root","");
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $stmt = $db->prepare("SELECT * FROM `database-news`");
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    print_r($result);
+
+    foreach($result as $key => $value) {
+        $id = $value['id'];
+        //echo $id;
+        $title = $value['title'];
+        //echo $title;
+    }
+
 } catch(Exception $e) {
-    echo "Unable to connect. ";
+    echo "Unable to connect. Error: ";
     echo $e->getMessage();
     exit;
 }
 
 echo "Connected to the database!";
+
+
 
 ?>
